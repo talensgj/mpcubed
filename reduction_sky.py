be476a5eca5740ba4bf6bddad6133d2d6cf1bf88
Geert Jan Talens <talens@strw.leidenuniv.nl>
2015-06-18 17:38:30 +0200

    End 18/06/15.

--- reduction_sky.py
+++ reduction_sky.py
@@ -47,12 +47,19 @@ error = make_array(etflux0, Nobs, time_id)
 binnum = healpy.ang2pix(16, (dec+90)*np.pi/180., ra*np.pi/180.)
 offset_binnum = np.amin(binnum)
 
-transmission, normalization, niter, chi2, sflags = make_transmission_map(data, error, binnum-offset_binnum)
+print len(np.unique(binnum)), np.ptp(binnum)
 
+sky, normalization, niter, chi2, sflags = make_transmission_map(data, error, binnum-offset_binnum)
+
+with h5py.File('/data2/talens/Oct2014LPE_Result.hdf5') as f:
+    tflux0 = f['Pipeline/tFlux0'].value
+    etflux0 = f['Pipeline/etFlux0'].value
+    myflags = f['Pipeline/MyFlags'].value
+    
 binnum = healpy.ang2pix(16, (np.repeat(dec, Nobs)+90)*np.pi/180., np.repeat(ra, Nobs)*np.pi/180.)
 _, binnum = np.unique(binnum, return_inverse=True)
 
-scurve = transmission[binnum, time_id]
+scurve = sky[binnum, time_id]
 myflags += 4*sflags[binnum, time_id]
 
 stflux0 = tflux0/scurve
@@ -65,3 +72,7 @@ with h5py.File('/data2/talens/Oct2014LPE_Result.hdf5') as f:
     grp.create_dataset('estFlux0', data=estflux0)
     grp.create_dataset('Sky', data=scurve)
     grp.create_dataset('MyFlags2', data=myflags)
+
+
+
+
