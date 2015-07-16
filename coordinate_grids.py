af1229f2d99c1059165b428fcfc70f65f0513a7f
Geert Jan Talens <talens@strw.leidenuniv.nl>
2015-07-16 17:46:38 +0200

    End 16/07/15.

---
+++ coordinate_grids.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import healpy
+
+import matplotlib.pyplot as plt
+
+class PolarGrid():
+    
+    def __init__(self, nx, ny):
+        
+        self.nx = nx
+        self.ny = ny
+        
+        self.bins1 = np.linspace(0, 360, self.nx+1)
+        self.bins2 = np.linspace(-90, 90, self.ny+1)
+        
+    def find_gridpoint(self, ra, dec):
+        
+        ind1 = np.searchsorted(self.bins1, ra, 'right')
+        ind2 = np.searchsorted(self.bins2, dec, 'right')
+        
+        if np.any(ind1==0) | np.any(ind1==self.nx+1):
+            print 'Warning 1'
+        if np.any(ind2==0) | np.any(ind2==self.ny+1):
+            print 'Warning 2'
+        
+        ind = np.ravel_multi_index([ind1,ind2], (self.nx+2, self.ny+2))
+        
+        count = np.bincount(ind)
+        count = self.put_values_on_grid(count)
+        
+        return ind, count
+        
+    def put_values_on_grid(self, values):
+        
+        array = np.zeros((self.nx+2)*(self.ny+2))
+        array[:len(values)] = values
+        array = array.reshape((self.nx+2, self.ny+2))
+    
+        return array
+    
+class CartesianGrid():
+    
+    def __init__(self, nx, ny, margin=0, Lx=4008, Ly=2672):
+        
+        self.nx = nx
+        self.ny = ny
+        
+        self.bins1 = np.linspace(margin, Lx-margin, self.nx+1)
+        self.bins2 = np.linspace(margin, Ly-margin, self.ny+1)
+        
+    def find_gridpoint(self, x, y):
+        
+        ind1 = np.searchsorted(self.bins1, x, 'right')
+        ind2 = np.searchsorted(self.bins2, y, 'right')
+        
+        if np.any(ind1==0) | np.any(ind1==self.nx+1):
+            print 'Warning 1'
+        if np.any(ind2==0) | np.any(ind2==self.ny+1):
+            print 'Warning 2'
+        
+        ind = np.ravel_multi_index([ind1,ind2], (self.nx+2, self.ny+2))
+    
+        count = np.bincount(ind)
+        count = self.put_values_on_grid(count)
+        
+        return ind, count
+        
+    def put_values_on_grid(self, values):
+        
+        array = np.zeros((self.nx+2)*(self.ny+2))
+        array[:len(values)] = values
+        array = array.reshape((self.nx+2, self.ny+2))
+        
+        return array
+    
+class HealpixGrid():
+    
+    def __init__(self, nside):
+        
+        self.nside = nside
+        self.npix = healpy.nside2npix(self.nside)
+    
+    def find_gridpoint(self, ra, dec):
+        
+        ind = healpy.ang2pix(self.nside, (dec+90)*np.pi/180., ra*np.pi/180.)
+        
+        count = np.bincount(ind)
+        count = self.put_values_on_grid(count)
+        
+        return ind, count
+        
+    def put_values_on_grid(self, values):
+        
+        array = np.zeros(self.npix)
+        array[:len(values)] = values
+        
+        return array
+
+ra = np.random.rand(5e6)*360
+dec = np.arccos(np.random.rand(5e6)*2-1)*180/np.pi-90
+
+pg = PolarGrid(135, 144)
+binnum, count = pg.find_gridpoint(ra, dec)
+
+count[count==0] = np.nan
+plt.imshow(count.T, aspect='auto', interpolation='None', origin='lower')
+plt.colorbar()
+plt.show()
+
+hg = HealpixGrid(32)
+binnum, count = hg.find_gridpoint(ra, dec)
+
+healpy.mollview(count)
+plt.show()
+
+
+
+
+
--- get_xytrans.py
+++ get_xytrans.py
@@ -9,6 +9,8 @@ import numpy as np
 import healpy
 from scipy.stats import binned_statistic_2d
 
+from time import time
+
 import matplotlib.pyplot as plt
 
 def make_idx(variable, range, nbins):
@@ -28,37 +30,28 @@ def make_idx(variable, range, nbins):
     
     return idx, length, offset
 
-def sysrem(ind1, ind2, values, errors, a1=None, a2=None, maxiter=50, eps=1e-3):
-    
-    if a1 is None and a2 is None:
-        
-        a2 = np.ones(len(np.bincount(ind2)))
-        
-    elif a1 is None:
-        
-        pass
-        
-    elif a2 is None:
-        
-        a2 = np.bincount(ind2, values*a1[ind1]/errors**2)/np.bincount(ind2, (a1**2)[ind1]/errors**2)
-        
-    else:
-        
-        print 'You cannot use initial guesses for both parameters.'
-        exit()
+def sysrem(ind1, ind2, values, errors, a2=None, maxiter=50, eps=1e-3):
     
     npoints = len(values)
-    npars = len(np.unique(ind1)) + len(np.unique(ind2))
+    npars1 = len(np.unique(ind1))
+    npars2 = len(np.unique(ind2))
+    npars = npars1 + npars2
+    
+    if a2 is None:
+        a2 = np.ones(npars2) # WRONG!!!
     
     niter = 0
     chisq = np.zeros(maxiter)
     chisq_crit = True
+    
+    s = values/errors**2
+    r = 1./errors**2
+    
     while (niter < maxiter) & (chisq_crit):
         
-        a1 = np.bincount(ind1, values*a2[ind2]/errors**2)/np.bincount(ind1, (a2**2)[ind2]/errors**2)
-        a2 = np.bincount(ind2, values*a1[ind1]/errors**2)/np.bincount(ind2, (a1**2)[ind1]/errors**2)
-        
-        chisq[niter] = np.nansum((values-a1[ind1]*a2[ind2])**2/errors**2)/(npoints-npars)
+        a1 = np.bincount(ind1, s*a2[ind2])/np.bincount(ind1, r*(a2**2)[ind2])
+        a2 = np.bincount(ind2, s*a1[ind1])/np.bincount(ind2, r*(a1**2)[ind1])
+        chisq[niter] = np.sum(r*(values-a1[ind1]*a2[ind2])**2)/(npoints-npars)
         
         if niter == 0:
             chisq_crit = True
@@ -69,11 +62,149 @@ def sysrem(ind1, ind2, values, errors, a1=None, a2=None, maxiter=50, eps=1e-3):
 
         niter += 1
     
-    chisq_pbin1 = np.bincount(ind1, (values-a1[ind1]*a2[ind2])**2/errors**2)
-    chisq_pbin2 = np.bincount(ind2, (values-a1[ind1]*a2[ind2])**2/errors**2)
+    chisq_pbin1 = np.bincount(ind1, r*(values-a1[ind1]*a2[ind2])**2)
+    chisq_pbin2 = np.bincount(ind2, r*(values-a1[ind1]*a2[ind2])**2)
     
     return a1, a2, niter, chisq[niter-1], chisq_pbin1, chisq_pbin2, npoints, npars
 
+def regrid(ind1, ind2, values, errors, a2):
+    
+    npoints = len(values)
+    npars = len(np.unique(ind1)) + len(np.unique(ind2))
+        
+    a1 = np.bincount(ind1, values*a2[ind2]/errors**2)/np.bincount(ind1, (a2**2)[ind2]/errors**2)
+        
+    chisq = np.nansum((values-a1[ind1]*a2[ind2])**2/errors**2)/(npoints-npars)
+    
+    chisq_pbin1 = np.bincount(ind1, (values-a1[ind1]*a2[ind2])**2/errors**2)
+    chisq_pbin2 = np.bincount(ind2, (values-a1[ind1]*a2[ind2])**2/errors**2)
+    
+    return a1, a2, chisq, chisq_pbin1, chisq_pbin2, npoints, npars
+    
+def polar_grid(ra, dec, nx, ny):
+    
+    ind1 = np.searchsorted(np.linspace(0, 360, nx+1), ra, 'right')
+    ind2 = np.searchsorted(np.linspace(-90, 90, ny+1), dec, 'right')
+    
+    ind = np.ravel_multi_index([ind1,ind2], (nx+2, ny+2))
+    
+    count = np.bincount(ind, minlength=(nx+2)*(ny+2))
+    count = count.reshape((nx+2, ny+2))
+    
+    return ind, count
+    
+def test_grid(values, nx, ny):
+    
+    array = np.zeros((nx+2)*(ny+2))
+    array[:len(values)] = values
+    
+    return array
+    
+class PolarGrid():
+    
+    def __init__(self, nx, ny):
+        
+        self.nx = nx
+        self.ny = ny
+        
+        self.bins1 = np.linspace(0, 360, self.nx+1)
+        self.bins2 = np.linspace(-90, 90, self.ny+1)
+        
+    def find_gridpoint(self, ra, dec):
+        
+        ind1 = np.searchsorted(self.bins1, ra, 'right')
+        ind2 = np.searchsorted(self.bins2, dec, 'right')
+        
+        ind = np.ravel_multi_index([ind1,ind2], (self.nx+2, self.ny+2))
+        
+        count = np.bincount(ind)
+        count = self.put_values_on_grid(count)
+        
+        return ind, count
+        
+    def put_values_on_grid(self, values):
+        
+        array = np.zeros((self.nx+2)*(self.ny+2))
+        array[:len(values)] = values
+        array = array.reshape((self.nx+2, self.ny+2))
+    
+        return array
+    
+class CartesianGrid():
+    
+    def __init__(self, nx, ny, margin=0, Lx=4008, Ly=2672):
+        
+        self.nx = nx
+        self.ny = ny
+        
+        self.bins1 = np.linspace(margin, Lx-margin, self.nx+1)
+        self.bins2 = np.linspace(margin, Ly-margin, self.ny+1)
+        
+    def find_gridpoint(self, x, y):
+        
+        ind1 = np.searchsorted(self.bins1, x, 'right')
+        ind2 = np.searchsorted(self.bins2, y, 'right')
+        ind = np.ravel_multi_index([ind1,ind2], (self.nx+2, self.ny+2))
+    
+        count = np.bincount(ind)
+        count = self.put_values_on_grid(count)
+        
+        return ind, count
+        
+    def put_values_on_grid(self, values):
+        
+        array = np.zeros((self.nx+2)*(self.ny+2))
+        array[:len(values)] = values
+        array = array.reshape((self.nx+2, self.ny+2))
+        
+        return array
+    
+class HealpixGrid():
+    
+    def __init__(self, nside):
+        
+        self.nside = nside
+        self.npix = healpy.nside2npix(self.nside)
+    
+    def find_gridpoint(self, ra, dec):
+        
+        ind = healpy.ang2pix(self.nside, (dec+90)*np.pi/180., ra*np.pi/180.)
+        
+        count = np.bincount(ind)
+        count = self.put_values_on_grid(count)
+        
+        return ind, count
+        
+    def put_values_on_grid(self, values):
+        
+        array = np.zeros(self.npix)
+        array[:len(values)] = values
+        
+        return array
+        
+    
+def healpy_grid(ra, dec, nside):
+    
+    npix = healpy.nside2npix(nside)
+    ind = healpy.ang2pix(nside, (dec+90)*np.pi/180., ra*np.pi/180.)
+    
+    count = np.bincount(ind, minlength=npix)
+    
+    return ind, count
+    
+def cartesian_grid(x, y, nx, ny, Lx=4008, Ly=2672, margin=0):
+    
+    ind1 = np.searchsorted(np.linspace(margin, Lx-margin, nx+1), x, 'right')
+    ind2 = np.searchsorted(np.linspace(margin, Ly-margin, ny+1), y, 'right')
+    
+    ind = np.ravel_multi_index([ind1,ind2], (nx+2, ny+2))
+    
+    count = np.bincount(ind, minlength=(nx+2)*(ny+2))
+    count = count.reshape((nx+2, ny+2))
+    
+    return ind, count
+    
+    
 def transmission(filename):
 
     with h5py.File(filename) as f:
@@ -104,90 +235,52 @@ def transmission(filename):
             flags[select[i]:select[i+1]] = data[ascc[i]]['flag']
             x[select[i]:select[i+1]] = data[ascc[i]]['x']
             y[select[i]:select[i+1]] = data[ascc[i]]['y']
+    
+    print 'Done reading.'
 
     dec_1 = np.copy(dec)
 
-    # 
+    # Sky coordinates of data.
     ha = np.mod(lst*15.-np.repeat(ra,Nobs), 360.)
     dec = np.repeat(dec, Nobs)
 
-    nside = 64
-    filename = 'Tvmag_20150203LPE_ns%i.hdf5'%nside
-
     # Create the indices.
-    npix = healpy.nside2npix(nside)
-    binnum = healpy.ang2pix(nside, (dec+90)*np.pi/180., ha*np.pi/180.)
     star_id = np.repeat(np.arange(len(ascc)), Nobs)
+    binnum, count = healpy_grid(ha, dec, 512)
+    
+    hg = HealpixGrid(512)
+    binnum1, count1 = hg.find_gridpoint(ha, dec)
+    
+    print np.percentile(count[count>0], 5), np.percentile(count[count>0], 95)
     
     # Remove bad data.
-    here, = np.where((flags < 1)&np.isfinite(eflux0))
+    here, = np.where(flags < 1)
     flux0 = flux0[here]
     eflux0 = eflux0[here]
-    binnum = binnum[here]
-    star_id = star_id[here]
     x = x[here]
     y = y[here]
-    #vmag = vmag[np.unique(star_id)]
-    
-    a1, a2, niter, chisq, chisq_pbin1, chisq_pbin2, npoints, npars = sysrem(star_id, binnum, flux0, eflux0, a1=1e7*10**(vmag/-2.5))
+    binnum = binnum[here]
+    star_id = star_id[here]
 
-    exit()
-    trans = np.full(npix, fill_value=np.nan)
-    trans[bins] = a_j[bins]
-
-    chisq = np.full(npix, fill_value=np.nan)
-    chisq[bins] = chisq_pbin[bins]
-        
-    count = np.full(npix, fill_value=np.nan)
-    count[bins] = np.bincount(binnum)[bins]
+    # Compute the transmission using sysrem.
+    a1, a2, niter, chisq, chisq_pbin1, chisq_pbin2, npoints, npars = sysrem(binnum, star_id, flux0, eflux0, a2=1e7*10**(vmag/-2.5))
     
-    print np.nanmean(count)
     
-    f = h5py.File(filename)
-    f.create_dataset('ra', data=ra[stars])
-    f.create_dataset('ascc', data=ascc[stars])
-    f.create_dataset('dec', data=dec_1[stars])
-    f.create_dataset('nobs', data=Nobs[stars])
-    f.create_dataset('vmag', data=vmag[stars])
-    f.create_dataset('bmag', data=bmag[stars])
-    f.create_dataset('blendvalue', data=blendval[stars])
-    f.create_dataset('flux', data=c_i[stars])
-    f.create_dataset('count', data=count)
-    f.create_dataset('trans', data=trans)
-    f.create_dataset('chisq', data=chisq)
-    f.close()
+    array = np.zeros(len(count))
+    array[:len(a1)] = a1
     
-    #  
-    nx = (200, 400, 800)
-    ny = (150, 300, 600)  
-    for nx, ny in zip(nx, ny):
-        print nx, ny
+    healpy.mollview(array)
+    plt.show()
     
-        count_xy, xedges, yedges, binnum = binned_statistic_2d(x, y, flux0, statistic='count', bins = [np.linspace(50,3958,nx+1), np.linspace(50,2622,ny+1)])
-        a_j = np.bincount(binnum, flux0*c_i[star_id]/eflux0**2)/np.bincount(binnum, (c_i**2)[star_id]/eflux0**2)
-        chisq_pbin = np.bincount(binnum, (flux0-c_i[star_id]*a_j[binnum])**2/eflux0**2)
-        
-        bins = np.unique(binnum)
-        npars = len(bins)+len(stars)
-        
-        trans_xy = np.full((nx+2)*(ny+2), fill_value=np.nan)
-        trans_xy[bins] = a_j[bins]
-        trans_xy = trans_xy.reshape((ny+2,nx+2))
-        
-        chisq_xy = np.full((nx+2)*(ny+2), fill_value=np.nan)
-        chisq_xy[bins] = chisq_pbin[bins]
-        chisq_xy = chisq_xy.reshape((ny+2,nx+2))
-        
-        count_xy = count_xy.T
-            
-        print np.nansum(chisq_xy)/(npoints-npars)
-        
-        f = h5py.File(filename)
-        grp = f.create_group('nx%iny%i'%(nx,ny))
-        grp.create_dataset('count', data=count_xy)
-        grp.create_dataset('trans', data=trans_xy)
-        grp.create_dataset('chisq', data=chisq_xy)
-        f.close()
+    stars = np.unique(star_id)
+    ratio = a2[stars]/(1e7*10**(vmag[stars]/-2.5))
+    
+    plt.plot(dec_1[stars], ratio, '.', alpha=.1)
+    plt.show()
+    
+    # Recompute the transmission on a new grid.
+    binnum, count = cartesian_grid(x, y, 800, 600, margin=50)
+    a1, a2, chisq, chisq_pbin1, chisq_pbin2, npoints, npars = regrid(binnum, star_id, flux0, eflux0, a2)
 
     return 0
 
