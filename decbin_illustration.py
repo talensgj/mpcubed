f0d906113bcb60f727db9eb54725da7abf33e094
Geert Jan Talens <talens@strw.leidenuniv.nl>
2015-10-01 12:16:14 +0200

    Clean-up of illustrations.

--- decbin_illustration.py
+++ decbin_illustration.py
@@ -4,21 +4,31 @@
 import h5py
 import numpy as np
 
-import intrarem
 from coordinate_grids import PolarGrid
-
 from index_functions import index_statistics
+
+import sysrem_dev
 
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
-
 from viridis import viridis 
-
+from matplotlib.ticker import FuncFormatter, MultipleLocator, NullLocator
 rcParams['xtick.labelsize'] = 'large'
 rcParams['ytick.labelsize'] = 'large'
 rcParams['axes.labelsize'] = 'x-large'
+rcParams['axes.titlesize'] = 'xx-large'
 rcParams['image.interpolation'] = 'none'
 rcParams['image.origin'] = 'lower'
+
+def periodic(x, pos):
+        'The two args are the value and tick position'
+        x = x%24
+        x1 = np.floor(x)
+        x2 = (x-np.floor(x))*60
+        
+        return '%02.fh%02.fm' % (x1, x2)
+
+formatter = FuncFormatter(periodic)
 
 pg1 = PolarGrid(13500, 720)
 pg2 = PolarGrid(270, 720)
@@ -29,15 +39,17 @@ with h5py.File('/data2/talens/Jul2015/fLC_20150716LPC.hdf5', 'r') as f:
     ra = f['header_table/ra'].value
     dec = f['header_table/dec'].value
     nobs = f['header_table/nobs'].value.astype('int')
+    vmag = f['header_table/vmag'].value
     
     decidx = pg1.find_decidx(dec)
     print decidx[ascc=='807144']
-    
+
     here = (decidx == 451)
     ascc = ascc[here]
     ra = ra[here]
     dec = dec[here]
     nobs = nobs[here]
+    vmag = vmag[here]
     
     lst = np.array([])
     y = np.array([])
@@ -54,105 +66,186 @@ with h5py.File('/data2/talens/Jul2015/fLC_20150716LPC.hdf5', 'r') as f:
         tmp = index_statistics(lc['lstidx']//50, lc['flux0'], statistic='std', keeplength=True)
         sflux = np.append(sflux, tmp)
     
-    ha = np.mod(lst*15.-np.repeat(ra, nobs), 360)
-    
-    haidx1 = pg1.find_raidx(ha)
-    haidx2 = pg2.find_raidx(ha)
-    staridx = np.repeat(np.arange(len(ascc)), nobs)
-    
-    here = (flux>0)&(sflux>0)
-    staridx = staridx[here]
-    haidx1 = haidx1[here]
-    haidx2 = haidx2[here]
-    y = y[here]
-    flux = flux[here]
-    eflux = eflux[here]
-    sflux = sflux[here]
-    
-    F1, T1, a1, b1, niter, chisq, npoints, npars = intrarem.trans_intrapixel(staridx, haidx1, haidx2, y, flux, sflux, verbose=True, eps=1e-6)
-    fit1 = F1[staridx]*T1[haidx1]*(a1[haidx2]*np.sin(2*np.pi*y)+b1[haidx2]*np.cos(2*np.pi*y)+1)
-    
-    F1 *= np.nanmax(T1)
-    T1 /= np.nanmax(T1)
-   
-    array1 = np.full((len(ascc), 13502), fill_value=np.nan)
-    array1[staridx, haidx1] = flux/(F1[staridx])
-    #array1 = array1[:,1:-1]
-    array1 = array1[np.argsort(ra)]
-    array1 = np.roll(array1, 13500/2, axis=1)
-    
-    array2 = np.full((len(ascc), 13502), fill_value=np.nan)
-    array2[staridx, haidx1] = (a1[haidx2]*np.sin(2*np.pi*y)+b1[haidx2]*np.cos(2*np.pi*y)+1)
-    #array2 = array2[:,1:-1]
-    array2 = array2[np.argsort(ra)]
-    array2 = np.roll(array2, 13500/2, axis=1)
-    
-    array3 = np.full((len(ascc), 13502), fill_value=np.nan)
-    array3[staridx, haidx1] = flux/(F1[staridx]*T1[haidx1])
-    #array3 = array3[:,1:-1]
-    array3 = array3[np.argsort(ra)]
-    array3 = np.roll(array3, 13500/2, axis=1)
-    
-    count = np.bincount(haidx1)
-    here = count>0
-    ha1 = pg1.find_ra(np.arange(len(T1)))/360*24
-    ha1 = np.mod(ha1-12, 24)-12
-    
-    array4 = array3/array2
-    
-    ind = np.arange(len(T1))
-    ind = np.mod(ind-13502/2, 13502)
-    
-    haidx1 = np.mod(haidx1-13502/2, 13502)
-    
-    plt.figure(figsize=(16,12))
-    
-    ax = plt.subplot(321)
-    plt.imshow(array1, aspect='auto', cmap=viridis, vmin=0, vmax=1)
-    cb = plt.colorbar()
-    plt.xlabel('HA [idx]')
-    cb.set_label('flux0/F')
-    
-    plt.subplot(322, sharex=ax)
-    plt.plot(ind, T1, '.', c='k')
-    plt.ylim(0, 1)
-    cb = plt.colorbar()
-    plt.xlabel('HA [idx]')
-    plt.ylabel('T')
-    
-    plt.subplot(323, sharex=ax, sharey=ax)
-    plt.imshow(array3, aspect='auto', cmap=viridis, vmin=.9, vmax=1.1)
-    cb = plt.colorbar()
-    plt.xlabel('HA [idx]')
-    cb.set_label('flux0/(F*T)')
-    
-    plt.subplot(324, sharex=ax, sharey=ax)
-    plt.imshow(array2, aspect='auto', cmap=viridis, vmin=.9, vmax=1.1)
-    cb = plt.colorbar()
-    plt.xlabel('HA [idx]')
-    cb.set_label('IP')
-    
-    plt.subplot(325, sharex=ax, sharey=ax)
-    plt.imshow(array3/array2, aspect='auto', cmap=viridis, vmin=.9, vmax=1.1)
-    cb = plt.colorbar()
-    plt.xlim(np.amin(haidx1)-.5, np.amax(haidx1)+.5)
-    plt.xlabel('HA [idx]')
-    cb.set_label('flux0/(F*T*IP)')
-    
-    plt.subplot(326)
-    plt.hist(array4[np.isfinite(array4)], bins=np.linspace(.5, 1.5, 200), histtype='step', lw=2, normed=True)
-    plt.xlabel('flux0/(F*T*IP)')
-    plt.ylabel('PDF')
-    
-    plt.tight_layout()
-    plt.show()
-    plt.close()
-    
-    
-    
-    
-    
+eflux = sflux
 
-        
-        
-        
+ha = np.mod(lst*15.-np.repeat(ra, nobs), 360)
+
+haidx1 = pg1.find_raidx(ha)
+haidx2 = pg2.find_raidx(ha)
+staridx = np.repeat(np.arange(len(ascc)), nobs)
+
+here = (flux > 0)&(eflux > 0)
+staridx = staridx[here]
+haidx1 = haidx1[here]
+haidx2 = haidx2[here]
+y = y[here]
+flux = flux[here]
+eflux = eflux[here]
+
+data = np.full((len(ascc), 13502), fill_value=np.nan)
+data[staridx, haidx1] = flux
+data = data[:,1:-1]
+data = np.roll(data, 13500/2, axis=1)
+data = data/np.nanmedian(data, axis=1, keepdims=True)
+
+sort = np.argsort(ra)
+data = data[sort]
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+ax.yaxis.set_major_locator(NullLocator())
+plt.title(r'16-07-2015 LPC, Dec = 22.50$^\circ$ - 22.75$^\circ$')
+plt.imshow(data, aspect='auto', cmap=viridis, extent=(-12,12,0,1), vmin=0, vmax=2.5)
+cb = plt.colorbar()
+plt.xlim(-3,3)
+cb.set_label('Normalized Flux')
+plt.xlabel('Hour Angle')
+plt.tight_layout()
+#plt.show()
+plt.savefig('Pipeline_part1_0_data.png')
+
+F, T, niter, chisq, npoints, npars = sysrem_dev.sysrem(staridx, haidx1, flux, eflux, verbose=True)
+
+x = np.arange(len(T))
+x = pg1.find_ra(x)
+x = np.mod(x-180, 360)-180
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+plt.title(r'$\rm{niter}=%i$, $\chi^{2}=%.2f$'%(niter, chisq))
+plt.plot(x/15, T, '.')
+plt.xlim(-3,3)
+plt.ylim(0, 2.5)
+plt.xlabel('Hour Angle')
+plt.ylabel('Transmission')
+#plt.show()
+plt.savefig('Pipeline_part1_1_transmission1.png')
+
+data = np.full((len(ascc), 13502), fill_value=np.nan)
+data[staridx, haidx1] = flux/(F[staridx]*T[haidx1])
+data = data[:,1:-1]
+data = np.roll(data, 13500/2, axis=1)
+data = data/np.nanmedian(data, axis=1, keepdims=True)
+
+sort = np.argsort(ra)
+data = data[sort]
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+ax.yaxis.set_major_locator(NullLocator())
+plt.title(r'16-07-2015 LPC, Dec = 22.50$^\circ$ - 22.75$^\circ$')
+plt.imshow(data, aspect='auto', cmap=viridis, extent=(-12,12,0,1), vmin=.8, vmax=1.2)
+cb = plt.colorbar()
+plt.xlim(-3,3)
+cb.set_label('Reduced Flux')
+plt.xlabel('Hour Angle')
+#plt.show()
+plt.savefig('Pipeline_part1_2_result.png')
+
+F, T, a, b, niter, chisq, npoints, npars = sysrem_dev.intrarem(staridx, haidx1, haidx2, y, flux, eflux, verbose=True)
+
+x = np.arange(len(T))
+x = pg1.find_ra(x)
+x = np.mod(x-180, 360)-180
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+plt.title(r'$\rm{niter}=%i$, $\chi^{2}=%.2f$'%(niter, chisq))
+plt.plot(x/15, T, '.')
+plt.xlim(-3,3)
+plt.ylim(0,2.5)
+plt.xlabel('Hour Angle')
+plt.ylabel('Transmission')
+#plt.show()
+plt.savefig('Pipeline_part1_3_transmission2.png')
+
+x = np.arange(len(a))
+x = pg2.find_ra(x)
+x = np.mod(x-180, 360)-180
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+plt.title(r'$\rm{niter}=%i$, $\chi^{2}=%.2f$'%(niter, chisq))
+plt.plot(x/15, a, '.', label='a')
+plt.plot(x/15, b, '.', label='b')
+plt.xlim(-3,3)
+plt.ylim(-.06, .06)
+plt.xlabel('Hour Angle')
+plt.ylabel('Amplitude')
+#plt.show()
+plt.savefig('Pipeline_part1_4_amplitudes.png')
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+plt.title(r'$\rm{niter}=%i$, $\chi^{2}=%.2f$'%(niter, chisq))
+plt.plot(x/15, np.sqrt(a**2+b**2), '.', label='a')
+plt.xlim(-3,3)
+plt.ylim(0, .06)
+plt.xlabel('Hour Angle')
+plt.ylabel('Amplitude')
+#plt.show()
+plt.savefig('Pipeline_part1_5_amplitude.png')
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+plt.title(r'$\rm{niter}=%i$, $\chi^{2}=%.2f$'%(niter, chisq))
+plt.plot(x/15, np.arctan(b/a), '.', label='a')
+plt.xlim(-3,3)
+plt.ylim(-np.pi/2, np.pi/2)
+plt.xlabel('Hour Angle')
+plt.ylabel('Phase')
+#plt.show()
+plt.savefig('Pipeline_part1_6_phase.png')
+
+data = np.full((len(ascc), 13502), fill_value=np.nan)
+data[staridx, haidx1] = (a[haidx2]*np.sin(2*np.pi*y)+b[haidx2]*np.cos(2*np.pi*y)+1)
+data = data[:,1:-1]
+data = np.roll(data, 13500/2, axis=1)
+data = data/np.nanmedian(data, axis=1, keepdims=True)
+
+sort = np.argsort(ra)
+data = data[sort]
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+ax.yaxis.set_major_locator(NullLocator())
+plt.title(r'$\rm{niter}=%i$, $\chi^{2}=%.2f$'%(niter, chisq))
+plt.imshow(data, aspect='auto', cmap=viridis, extent=(-12,12,0,1), vmin=.8, vmax=1.2)
+cb = plt.colorbar()
+plt.xlim(-3,3)
+cb.set_label('Intrapixel Variations')
+plt.xlabel('Hour Angle')
+plt.tight_layout()
+#plt.show()
+plt.savefig('Pipeline_part1_7_intrapixel.png')
+
+data = np.full((len(ascc), 13502), fill_value=np.nan)
+data[staridx, haidx1] = flux/(F[staridx]*T[haidx1]*(a[haidx2]*np.sin(2*np.pi*y)+b[haidx2]*np.cos(2*np.pi*y)+1))
+data = data[:,1:-1]
+data = np.roll(data, 13500/2, axis=1)
+data = data/np.nanmedian(data, axis=1, keepdims=True)
+
+sort = np.argsort(ra)
+data = data[sort]
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+ax.yaxis.set_major_locator(NullLocator())
+plt.title(r'16-07-2015 LPC, Dec = 22.50$^\circ$ - 22.75$^\circ$')
+plt.imshow(data, aspect='auto', cmap=viridis, extent=(-12,12,0,1), vmin=.8, vmax=1.2)
+cb = plt.colorbar()
+plt.xlim(-3,3)
+cb.set_label('Reduced Flux')
+plt.xlabel('Hour Angle')
+plt.tight_layout()
+#plt.show()
+plt.savefig('Pipeline_part1_8_result2.png')
--- skybin_illustration.py
+++ skybin_illustration.py
@@ -4,22 +4,31 @@
 import h5py
 import numpy as np
 
-from sysrem import sysrem
 from coordinate_grids import HealpixGrid
-
 from index_functions import index_statistics
+
+import sysrem_dev
 
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
-
 from viridis import viridis 
-
+from matplotlib.ticker import FuncFormatter, MultipleLocator, NullLocator
 rcParams['xtick.labelsize'] = 'large'
 rcParams['ytick.labelsize'] = 'large'
 rcParams['axes.labelsize'] = 'x-large'
+rcParams['axes.titlesize'] = 'xx-large'
 rcParams['image.interpolation'] = 'none'
 rcParams['image.origin'] = 'lower'
 
+def periodic(x, pos):
+        'The two args are the value and tick position'
+        x = x%24
+        x1 = np.floor(x)
+        x2 = (x-np.floor(x))*60
+        
+        return '%02.fh%02.fm' % (x1, x2)
+
+formatter = FuncFormatter(periodic)
 hg = HealpixGrid(8)
 
 with h5py.File('/data2/talens/Jul2015/fLC_20150714LPC.hdf5', 'r') as f, h5py.File('/data2/talens/Jul2015/red_20150714LPC.hdf5', 'r') as g:
@@ -41,66 +50,81 @@ with h5py.File('/data2/talens/Jul2015/fLC_20150714LPC.hdf5', 'r') as f, h5py.Fil
     lstidx = np.array([])
     flux = np.array([])
     eflux = np.array([])
+    sflux = np.array([])
     for i in range(len(ascc)):
         lc = f['data/'+ascc[i]]
         rc = g['data/'+ascc[i]]
         lstidx = np.append(lstidx, lc['lstidx'])
         flux = np.append(flux, rc['ipcflux0'])
-        eflux = np.append(eflux, rc['sipcflux0'])
-        #sflux = index_statistics(lc['lstidx']//50, lc['flux0'], statistic='std', keeplength=True)
-        #eflux = np.append(eflux, sflux)
+        eflux = np.append(eflux, rc['eipcflux0'])
+        sflux = np.append(sflux, rc['sipcflux0'])
     
-    lstidx = lstidx.astype('int')
-    staridx = np.repeat(np.arange(len(ascc)), nobs)
-    
-    here = (flux>0)&(eflux>0)
-    staridx = staridx[here]
-    lstidx = lstidx[here]
-    flux = flux[here]
-    eflux = eflux[here]
-    
-    F, S = sysrem(staridx, lstidx, flux, eflux, verbose=True, eps=1e-6)[:2]
-    
-    F *= np.nanmedian(S)
-    S /= np.nanmedian(S)
-   
-    array1 = np.full((len(ascc), 13500), fill_value=np.nan)
-    array1[staridx, lstidx] = flux/(F[staridx])
-    array1 = array1[np.argsort(ra)]
-    
-    array2 = np.full((len(ascc), 13500), fill_value=np.nan)
-    array2[staridx, lstidx] = flux/(F[staridx]*S[lstidx])
-    array2 = array2[np.argsort(ra)]
-    
-    plt.figure(figsize=(16,8))
-    ax = plt.subplot(221)
-    plt.imshow(array1, aspect='auto', cmap=viridis, vmin=.5, vmax=1.5)
-    cb = plt.colorbar()
-    cb.set_label('flux0/F')
-    plt.xlabel('LST [idx]')
-    plt.subplot(222, sharex=ax)
-    plt.plot(S, '.', c='k')
-    plt.ylim(0.5,1.5)
-    plt.xlabel('LST [idx]')
-    plt.ylabel('S')
-    plt.subplot(223, sharex=ax, sharey=ax)
-    plt.imshow(array2, aspect='auto', cmap=viridis, vmin=.5, vmax=1.5)
-    cb = plt.colorbar()
-    plt.xlabel('LST [idx]')
-    cb.set_label('flux0/(F*T)')
-    plt.xlim(np.amin(lstidx)-.5, np.amax(lstidx)+.5)
-    plt.subplot(224)
-    plt.hist(array2[np.isfinite(array2)], bins=np.linspace(0,2,200), histtype='step', lw=2, normed=True)
-    plt.xlabel('flux0/(F*T)')
-    plt.ylabel('PDF')
-    plt.tight_layout()
-    plt.show()
-    plt.close()
-    
-    
-    
-    
+eflux = sflux
 
-        
-        
-        
+lstidx = lstidx.astype('int')
+staridx = np.repeat(np.arange(len(ascc)), nobs)
+
+here = (flux>0)&(eflux>0)
+staridx = staridx[here]
+lstidx = lstidx[here]
+flux = flux[here]
+eflux = eflux[here]
+
+data = np.full((len(ascc), 13500), fill_value=np.nan)
+data[staridx, lstidx] = flux
+data = data/np.nanmedian(data, axis=1, keepdims=True)
+
+sort = np.argsort(ra)
+data = data[sort]
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+ax.yaxis.set_major_locator(NullLocator())
+plt.title(r'14-07-2015 LPC')
+plt.imshow(data, aspect='auto', cmap=viridis, extent=(0,24,0,1), vmin=0, vmax=1.2)
+cb = plt.colorbar()
+plt.xlim(16.5,23)
+cb.set_label('Normalized Flux')
+plt.xlabel('Local Sidereal Time')
+plt.tight_layout()
+#plt.show()
+plt.savefig('Pipeline_part2_0_data.png')
+
+F, S, niter, chisq, npoints, npars = sysrem_dev.sysrem(staridx, lstidx, flux, eflux, verbose=True)
+
+x = np.arange(len(S))
+x = x/13500.*24.
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+plt.title(r'$\rm{niter}=%i$, $\chi^{2}=%.2f$'%(niter, chisq))
+plt.plot(x, S, '.')
+plt.xlim(16.5,23)
+plt.ylim(0,1.2)
+plt.xlabel('Local Sidereal Time')
+plt.ylabel('Sky')
+#plt.show()
+plt.savefig('Pipeline_part2_1_sky.png')
+
+data = np.full((len(ascc), 13500), fill_value=np.nan)
+data[staridx, lstidx] = flux/(F[staridx]*S[lstidx])
+data = data/np.nanmedian(data, axis=1, keepdims=True)
+
+sort = np.argsort(ra)
+data = data[sort]
+
+plt.figure(figsize=(12,6))
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+ax.yaxis.set_major_locator(NullLocator())
+plt.title(r'14-07-2015 LPC')
+plt.imshow(data, aspect='auto', cmap=viridis, extent=(0,24,0,1), vmin=0, vmax=1.2)
+cb = plt.colorbar()
+plt.xlim(16.5,23)
+cb.set_label('Reduced Flux')
+plt.xlabel('Local Sidereal Time')
+plt.tight_layout()
+#plt.show()
+plt.savefig('Pipeline_part2_3_result.png')
