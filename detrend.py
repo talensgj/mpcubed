2b25154237be81e145c6c1f96cb5afc20ed7c4a3
Geert Jan Talens <talens@strw.leidenuniv.nl>
2016-03-08 17:29:37 +0100

    Several new possible methods of detrending added and tested.

--- detrend.py
+++ detrend.py
@@ -32,69 +32,81 @@ def masc_harmonic(jdmid, lst, value, weights, Pjd, njd, Plst, nlst, cnst=False):
     chisq = np.sum(chisq)
     
     return pars, fit, chisq
+    
+    
+def hybrid(jdmid, lstidx, mag, weights, step, ns, maxiter=10):
+    
+    freqs = fourier.fftfreq(step, ns)
+    freqs = freqs[freqs < 1/6.]
+    freqs = np.append(np.amin(freqs)/2, freqs)
+    mat = fourier.fourier_mat(jdmid, freqs)
+    
+    #x = jdmid[:,None]/np.amax(jdmid)
+    #mat = np.hstack([x, x**2, mat])
+    
+    fit2 = 0
+    for niter in range(maxiter):
+    
+        pars1 = fourier.fit_mat(mag - fit2, weights, mat)
+        fit1 = np.dot(mat, pars1)
+        
+        x0 = np.bincount(lstidx, weights)
+        x1 = np.bincount(lstidx, weights*(mag - fit1))
 
-#def masc_harmonic2(jdmid, lst, value, error, stepjd, njd, steplst, nlst, cnst=False):
+        pars2 = x1/x0
+        fit2 = pars2[lstidx]
+        
+    chisq = weights*(mag - fit1 - fit2)**2.
+    chisq = np.sum(chisq)
     
-    #freq_jd = fourier.fftfreq(stepjd, njd, False)
-    #freq_lst = fourier.fftfreq(steplst, nlst, False)
+    pars = np.append(pars1, pars2[np.unique(lstidx)]) 
+    
+    return pars, fit1, fit2, chisq
 
-    #freq_jd = freq_jd[freq_jd < 1/2.]
-    #freq_lst = freq_lst#[freq_lst < 1/.5]
-
-    #mat_jd = fourier.fourier_mat(jdmid, freq_jd)
-    #mat_lst = fourier.fourier_mat(lst, freq_lst)
+def iterative_detrending(jdmid, mag, weights, step, ns, maxiter=10):
     
-    #mat = np.hstack([mat_jd, mat_lst])
-    #if cnst: 
-        #mat = np.hstack([np.ones((len(value),1)), mat])
+    from scipy.signal import lombscargle
     
-    #pars = fourier.fit_mat(value, error, mat)
-    #fit = np.dot(mat, pars)
-    
-    #chisq = (value - fit)**2/error**2
-    #chisq = np.sum(chisq)
-    
-    #return chisq, pars, fit, mat
-
-#def iterative_detrending(jdmid, mag, emag, step, ns, maxiter=10):
-    
-    #from scipy.signal import lombscargle
-    
+    freqs1 = fourier.fftfreq(step, ns)
+    freqs2 = fourier.frequencies(.9972695664, 20)
+    freqs = np.append(freqs1, freqs2)
     #freqs = fourier.fftfreq(step, ns)
-    #freqs = freqs[freqs <= 24./2.]
-    #normval = jdmid.shape[0]
+    freqs = freqs[freqs < 12.]
+    normval = jdmid.shape[0]
     
-    #use_args = np.array([], dtype='int')
-    #fit = 0
+    use_args = np.array([], dtype='int')
+    fit = 0
     
-    #for niter in range(maxiter):
+    for niter in range(maxiter):
         
-        ## Compute the normalized Lomb-Scargle periodogram.
-        #pgram = lombscargle(jdmid, mag.astype('float64') - fit, 2*np.pi*freqs)
-        #pgram = np.sqrt(4.*pgram/normval)
+        # Compute the normalized Lomb-Scargle periodogram.
+        pgram = lombscargle(jdmid, mag.astype('float64') - fit, 2*np.pi*freqs)
+        pgram = np.sqrt(4.*pgram/normval)
         
-        ## Find the peak in the periogram.
-        #arg = np.argmax(pgram)
+        # Find the peak in the periogram.
+        arg = np.argmax(pgram)
         
-        ## Add the corresponding frequency to the fit.
-        #if arg in use_args:
-            #print 'Frequency already in use.'
-            #break
-        #elif (pgram[arg] > .001):
-            #use_args = np.append(use_args, arg)
-        #else:
-            #print 'Small amplitude.'
-            #break
+        # Add the corresponding frequency to the fit.
+        if arg in use_args:
+            print 'Frequency already in use.', 1./freqs[arg]
+            break
+        elif (pgram[arg] > np.mean(pgram)):
+            use_args = np.append(use_args, arg)
+        else:
+            print 'Small amplitude.'
+            break
         
-        ## Fit the data.
-        #mat = fourier.fourier_mat(jdmid, freqs[use_args])
-        #pars = fourier.fit_mat(mag, emag, mat)
-        #fit = np.dot(mat, pars)
+        # Fit the data.
+        mat = fourier.fourier_mat(jdmid, freqs[use_args])
+        pars = fourier.fit_mat(mag, weights, mat)
+        fit = np.dot(mat, pars)
         
-    #chisq = (mag - fit)**2/emag**2
-    #chisq = np.sum(chisq)
+    chisq = weights*(mag - fit)**2
+    chisq = np.sum(chisq)
     
-    #return chisq, pars, fit
+    print 1/freqs[use_args]
+    
+    return pars, fit, chisq
 
 
 def main():
