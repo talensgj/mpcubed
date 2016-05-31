88bd7aaff9ce8a75a5c9909122a01dd2f4a5319e
Geert Jan Talens <talens@strw.leidenuniv.nl>
2016-05-31 17:26:37 +0200

    Working toward possible distribution.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "mpcubed",
+    version = "1.0.0",
+    author = "Geert Jan Talens",
+    author_email = "talens@strw.leidenuniv.nl",
+    description = "The MASCARA Post-Processing Pipeline.",
+    license = "CCC",
+    packages = find_packages(),
+    long_description = read('README'),
+)
