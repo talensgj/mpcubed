2549e08d2348aa852b6aaa0844cb404c8367fe51
Geert Jan Talens <talens@strw.leidenuniv.nl>
2016-12-19 11:23:41 +0100

    Major update to transit fitting routines.

--- mpcubed/models/transit.py
+++ mpcubed/models/transit.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import occultquad
 
 def box(t, P, Tp, delta, eta, m=0.):
     """ Model the transit as a square dip in the lightcurve.
@@ -78,5 +79,103 @@ def softbox_chisq(pars, t, y, yerr):
     
     y0 = softbox(t, *pars)
     chisq = (y - y0)**2/yerr**2
+    chisq = np.sum(chisq)
+
+    if not np.isfinite(chisq):
+        chisq = 1e24
     
-    return np.sum(chisq)
+    return chisq
+
+def projected_seperation(t, t0, P, a, b):    
+    """ Compute projected seperation for circular orbits. """        
+    
+    # Compute the phase and projected separation.
+    phase = 2.*np.pi*(t - t0)/P
+    z = a*np.sqrt(np.sin(phase)**2. + (b/a)**2.*np.cos(phase)**2.)
+
+    return phase/(2.*np.pi), z
+
+def circ_model(t, Tp, P, tt, b, p, u1, u2):
+    """ Mandel & Agol model for circular transits. """    
+    
+    # Compute the semi-major axis using Seager 2003 eq 8.
+    x = np.sin(tt*np.pi/P)**2.
+    a = np.sqrt(((1. + p)**2. - (b**2.)*(1. - x))/x) 
+    print a
+    # Calculate the model lightcurve.
+    phase, z = projected_seperation(t, Tp, P, a, b)
+    mod = occultquad.occultquad(z, u1, u2, p)[0]
+    mod = np.where(np.abs(phase%1. - .5) < .25, 1., mod) # No secondary eclipse.
+        
+    return phase, mod
+
+def circ_chisq(pars, t, y, yerr, u1, u2, magnitudes=True):
+    """ Compute the chi-squared for circular orbits. """    
+    
+    Tp, P, tt, b, p = pars
+    weights = 1./yerr**2.    
+    
+    # Evaluate the model.
+    phase, mod = circ_model(t, Tp, P, tt, b, p, u1, u2)
+
+    # Evaluate the baseline.
+    if magnitudes:
+        mod = -2.5*np.log10(mod)
+        m0 = np.sum(weights*(y - mod))/np.sum(weights)
+        chisq = weights*(y - mod - m0)**2.
+    else:
+        f0 = np.sum(weights*y*mod)/np.sum(weights*mod**2.)
+        chisq = weights*(y - f0*mod)
+    
+    # Compute the chi-squared.
+    chisq = np.sum(chisq) 
+   
+    # Minimization routines do not work with infinities.
+    if not np.isfinite(chisq):
+        chisq = 1e24
+    
+    return chisq
+    
+def circ_lnlike(pars, t, y, yerr, u1, u2, magnitudes=True):
+    """ Compute the log-likelihood for circular orbits. """     
+    
+    Tp, P, tt, b, p = pars
+    weights = 1./yerr**2.    
+    
+    # Evaluate the model.
+    phase, mod = circ_model(t, Tp, P, tt, b, p, u1, u2)
+    
+    # Evaluate the baseline.
+    if magnitudes:
+        mod = -2.5*np.log10(mod)
+        m0 = np.sum(weights*(y - mod))/np.sum(weights)
+        lnlike = weights*(y - mod - m0)**2. - np.log(2.*np.pi*yerr**2.)  
+    else:
+        f0 = np.sum(weights*y*mod)/np.sum(weights*mod**2.)
+        lnlike = weights*(y - f0*mod)**2. - np.log(2.*np.pi*yerr**2.)  
+    
+    # Compute the log-likelihood.
+    lnlike = -.5*np.sum(lnlike)    
+    
+    return lnlike
+    
+def circ_lnprior(pars):
+    
+    b = pars[3]
+    p = pars[4]
+    
+    if not (0 < b < (1 + p)):
+        return -np.inf
+    
+    return 0.
+    
+def circ_lnprob(pars, t, y, yerr, u1, u2, magnitudes=True):
+    
+    lp = circ_lnprior(pars)
+    
+    if not np.isfinite(lp):
+        return -np.inf
+        
+    lnlike = circ_lnlike(pars, t, y, yerr, u1, u2, magnitudes)        
+        
+    return lp + lnlike
