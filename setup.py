52bba01e5773c126ab89fa5064acfbd5c99037d1
Geert Jan Talens <talens@astro.umontreal.ca>
2021-08-02 14:34:25 -0400

    Bug fix to making the diagnostic figures to deal with large data volumes.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name = "mpcubed",
-    version = "3.1.1",
+    version = "3.1.2",
     author = "Geert Jan Talens",
     author_email = "talens@strw.leidenuniv.nl",
     description = "The MASCARA Post-Processing Pipeline.",
