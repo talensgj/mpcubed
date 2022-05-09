bc20b596a0443b0a8a62da025b3766e54e41a318
talensgj <talensgj@gmail.com>
2022-05-09 17:03:35 -0400

    Add new entry point and bump version number.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name = "mpcubed",
-    version = "3.1.2",
+    version = "3.2.0",
     author = "Geert Jan Talens",
     author_email = "talens@strw.leidenuniv.nl",
     description = "The MASCARA Post-Processing Pipeline.",
@@ -19,6 +19,7 @@ setup(
     entry_points={'console_scripts': 
         ['merge_files = mpcubed.io:main',
          'run_calibration = mpcubed.calibration.cdecor:main',
+         'run_polar_calibration = mpcubed.calibration.cdecor:cmd_polar_calibration',
          'figs_calibration = mpcubed.calibration.figures:main',
          'run_boxlstsq = mpcubed.detection.boxlstsq:main',
          'figs_boxlstsq = mpcubed.detection.figures:main']}
